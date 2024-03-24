40c4372aa96c588dd855e50d4acbf2a637f98451
mussaratshamsher <mussaratskhan@github.com>
2024-03-24 01:19:11 -0700

    used chalk to make programm attractive

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 #! /usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
 //1 computer 'll generate a number
 // 2. user input for guessing number
 // 3. compare user input with computer generated number & show result
-const randomNumber = Math.floor(Math.random() * 6 + 1);
+//const randomNumber = Math.floor(Math.random() * 6+1);
+const randomNumber = 9;
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number:",
+        message: chalk.green('Please guess a number 1 to 9'),
     }
 ]);
 if (answers.userGuessedNumber === randomNumber) {
-    console.log("Congratulations! You guessed right number.");
+    console.log(chalk.yellowBright('Congratulations! You guessed right number.'));
 }
 else {
-    console.log("Sorry! you guessed wrong number.");
+    console.log(chalk.redBright('Sorry! you guessed wrong number. '));
 }
 ;
